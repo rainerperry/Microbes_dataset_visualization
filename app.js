693acafa7d607d2c6d23e9cd17f817bea897752a
rainerperry <68960541+rainerperry@users.noreply.github.com>
2020-12-09 16:56:03 -0500

    final

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 
 
 
-
+function init() {
 d3.json("data/samples.json").then((samples) => {
 
     //check data from json
@@ -23,23 +23,27 @@ d3.json("data/samples.json").then((samples) => {
 
     });
 
-   buildtable(samplenames[0])
-
+   buildtable(samplenames[0]);
+   dataDisplay(samplenames[0])
 });
+}
 
 //prepare plots
 
 function buildtable(sampleID) {
 
     d3.json("data/samples.json").then(function(sampleList) {
+        var chartdata = sampleList.samples    
+        var filterdata = chartdata.filter(x => x.id == sampleID)
+        console.log(filterdata[0])
 
         //get top 10 OTUs
-        var otuId = sampleList.samples[0].otu_ids.slice(0,10);
+        var otuId = filterdata[0].otu_ids.slice(0,10).map(x=> `OTU ${x}`).reverse();
         console.log(otuId)
-        var sampleValues = sampleList.samples[0].sample_values.slice(0, 10);
-        console.log(SampleValues)
-        var otuLabels = sampleList.samples[0].otu_labels.slice(0, 10);
-        console.log(otuLabels)
+        var sampleValues = filterdata[0].sample_values.slice(0, 10).reverse();
+        console.log(sampleValues)
+        var otuLabels = filterdata[0].otu_labels.slice(0, 10).reverse();
+        console.log(otuLabels)``
 
         //get OTUs in ascending order:
         //code here???
@@ -47,74 +51,73 @@ function buildtable(sampleID) {
         //build traces
         var trace1 = {
         x: sampleValues,
-        y: otuId
+        y: otuId,
         type: "bar",
         orientation: "h",
         color: "blue"
         }
-    };
 
-    // data & laoyout variable
+        // data & laoyout variable
 
+        
+        var data1 = [trace1];
+
+        var layout1 = {
+            title: "Top 10 OTU per Individual",
+            //yaxis: {tickmode: "linear"},
+            margin: {
+            l: 100,
+            r: 100,
+            t: 100,
+            b: 100
+            }
+        };
+        
+        //bar plot
+        Plotly.newPlot("bar", data1, layout1);
+
+        // data & layout for bubble plot
+
+        var trace2 = {
+            x: filterdata[0].otu_ids,
+            y: filterdata[0].sample_values,
+            mode: 'markers',
+            marker: {
+            size: filterdata[0].sample_values,
+            color: filterdata[0].otu_ids
+            },
+            text: filterdata[0].otu_labels
+        };
+
+    //data & layout variables
     
-    var data1 = [trace1];
-
-    var layout1 = {
-        title: "Top 10 OTU per Individual",
-        yaxis: {tickmode: "linear"},
-        margin: {
-        l: 100,
-        r: 100,
-        t: 100,
-        b: 100
-        }
-    };
+        var data2 = [trace2];
     
-    //bar plot
-    Plotly.newPlot("bar", data1, laoyout1);
+        var layout2 = {
+            xaxis: {title: "OTU ID"},
+            // yaxis??
+            height: 600,
+            width: 1000
+        };
+    
+        //bubble graph
+        Plotly.newPlot('bubble', data2, layout2);
+    });
+}
 
-    // data & layout for bubble plot
-
-    var trace2 = {
-        x: sampleList.samples[0].otu_ids,
-        y: sampleList.samples[0].sample_values,
-        mode: 'markers',
-        marker: {
-        size: samples.sample_values,
-        color: samples.otu_ids
-        },
-        text: samples.otu_labels
-    };
-
-  //data & layout variables
-  
-    var data2 = [trace2];
-  
-    var layout2 = {
-        xaxis: {title: "OTU ID"},
-        // yaxis??
-        height: 600,
-        width: 1000
-    };
-  
-    //bubble graph
-    Plotly.newPlot('bubble', data2, layout2);
-
-    };
-};
 
 //4. Function to filter & display the sample metadata in box "Demographic Info" 
 
 function dataDisplay(demoInfo) {
 
     d3.json("data/samples.json").then((data) => {
-        var samplemeta = samples.metadata;
+        var samplemeta = data.metadata;
         var location = d3.select("#sample-metadata");
 
         console.log(samplemeta)
-        var filterdata = samplemeta.filter(x => x.id == sampleID)
+        var filterdata = samplemeta.filter(x => x.id == demoInfo)
         console.log(filterdata[0])
-
+        location.html("")
         Object.entries(filterdata[0]).forEach(([key, value]) => {
             var row = location.append("tr");
             row.append("td").html(`<strong><font size = '1'>${key}</font></strong>:`);
@@ -123,16 +126,16 @@ function dataDisplay(demoInfo) {
     });
 };
 
-// function optionChanged(newSample) {
-//     buildtable(newSample);
+function optionChanged(newSample) {
+    buildtable(newSample);
 
-//    //buildCharts(newSample);
-//  }
+    dataDisplay(newSample);
+ }
 
 
 //Update the plots each time a new sample is selected
 
 
-
+init()
 
 
