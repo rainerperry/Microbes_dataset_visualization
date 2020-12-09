f0cb77ca6532ac6f3329ba53c6463d103116f04a
rainerperry <68960541+rainerperry@users.noreply.github.com>
2020-12-09 13:53:39 -0500

    Wed changes

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
+
+
+
+
+
+
+
+
+
 d3.json("data/samples.json").then((samples) => {
 
     //check data from json
     console.log(samples)
+
+
+//populate drop down menu with sample names
 
     var samplenames = samples.names
     var location = d3.select("#selDataset");
@@ -11,17 +23,92 @@ d3.json("data/samples.json").then((samples) => {
 
     });
 
-
    buildtable(samplenames[0])
 
 });
 
+//prepare plots
+
 function buildtable(sampleID) {
 
-    d3.json("data/samples.json").then((samples) => {
+    d3.json("data/samples.json").then(function(sampleList) {
+
+        //get top 10 OTUs
+        var otuId = sampleList.samples[0].otu_ids.slice(0,10);
+        console.log(otuId)
+        var sampleValues = sampleList.samples[0].sample_values.slice(0, 10);
+        console.log(SampleValues)
+        var otuLabels = sampleList.samples[0].otu_labels.slice(0, 10);
+        console.log(otuLabels)
+
+        //get OTUs in ascending order:
+        //code here???
+
+        //build traces
+        var trace1 = {
+        x: sampleValues,
+        y: otuId
+        type: "bar",
+        orientation: "h",
+        color: "blue"
+        }
+    };
+
+    // data & laoyout variable
 
     
-        var samplemeta = samples.metadata
+    var data1 = [trace1];
+
+    var layout1 = {
+        title: "Top 10 OTU per Individual",
+        yaxis: {tickmode: "linear"},
+        margin: {
+        l: 100,
+        r: 100,
+        t: 100,
+        b: 100
+        }
+    };
+    
+    //bar plot
+    Plotly.newPlot("bar", data1, laoyout1);
+
+    // data & layout for bubble plot
+
+    var trace2 = {
+        x: sampleList.samples[0].otu_ids,
+        y: sampleList.samples[0].sample_values,
+        mode: 'markers',
+        marker: {
+        size: samples.sample_values,
+        color: samples.otu_ids
+        },
+        text: samples.otu_labels
+    };
+
+  //data & layout variables
+  
+    var data2 = [trace2];
+  
+    var layout2 = {
+        xaxis: {title: "OTU ID"},
+        // yaxis??
+        height: 600,
+        width: 1000
+    };
+  
+    //bubble graph
+    Plotly.newPlot('bubble', data2, layout2);
+
+    };
+};
+
+//4. Function to filter & display the sample metadata in box "Demographic Info" 
+
+function dataDisplay(demoInfo) {
+
+    d3.json("data/samples.json").then((data) => {
+        var samplemeta = samples.metadata;
         var location = d3.select("#sample-metadata");
 
         console.log(samplemeta)
@@ -33,22 +120,19 @@ function buildtable(sampleID) {
             row.append("td").html(`<strong><font size = '1'>${key}</font></strong>:`);
             row.append('td').html(`<font size ='1'>${value}</font>`);
           });
-    })
+    });
 };
 
-function optionChanged(newSample) {
-    buildtable(newSample);
-   //buildCharts(newSample);
- }
+// function optionChanged(newSample) {
+//     buildtable(newSample);
 
-    //  Create the Traces
+//    //buildCharts(newSample);
+//  }
 
-  
-//     // Create the data array for the plot
-// 
-  
-//     // Define the plot layout
-//   
-//     // Plot the chart to a div tag with id "plot"
-//     Plotly.newPlot("plot", data, layout);
-//
+
+//Update the plots each time a new sample is selected
+
+
+
+
+
